2a6a1ec0c2c8d68d6da07b2a2c4e9b120aaad4f9
Abhishekk24 <123966857+Abhishekk24@users.noreply.github.com>
2023-12-08 00:03:03 +0530

    Updated

--- BS/FirstandLastOcc.java
+++ BS/FirstandLastOcc.java
@@ -5,38 +5,43 @@ import java.util.List;
 
 public class FirstandLastOcc {
     static int[] search(int[] arr, int target) {
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
         List<Integer> list = new ArrayList<>();
         int low = 0;
         int high = arr.length - 1;
-        int[] res = new int[2];
+        int[] res = new int[]{-1,-1};
          int indx=0;
         while (low <= high) {
             int mid = (low + high) / 2;
             if (arr[mid] == target) {
-                list.add(mid - 1);
-                list.add(mid);
-               
-                for(int nums:list){
-                    res[indx++] = nums;
-                }
-                return res;
+               res[0] = mid;
+               high = mid-1;
+
             } else if (target < arr[mid]) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        list.add(-1);
-        list.add(-1);
-        for(int nums:list){
-                    res[indx++] = nums;
-                }
-                return res;
+        low =0;
+        high= arr.length-1;
+         while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+               res[1] = mid;
+               low = mid+1;
+
+            } else if (target < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 5, 7, 7, 8, 8, 10 };
-        System.out.println("First occurrence of the element is at index: " + search(arr, 8));
+        int[] arr = { 5,7,7,8,8,9};
+        System.out.println("First occurrence of the element is at index: " + Arrays.toString(search(arr, 8)));
     }
 }
